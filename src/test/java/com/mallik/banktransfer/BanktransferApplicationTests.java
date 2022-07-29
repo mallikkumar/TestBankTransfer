983a8527c1de4801c76413238829ade2fd13ae05
Surya Mallik Kumar <32264714+mallikkumar@users.noreply.github.com>
2022-07-29 22:53:19 +0200

    Updated the test cases to add more details

--- src/test/java/com/mallik/banktransfer/BanktransferApplicationTests.java
+++ src/test/java/com/mallik/banktransfer/BanktransferApplicationTests.java
@@ -7,9 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,7 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mallik.banktransfer.exception.InsufficientFundsException;
 import com.mallik.banktransfer.exception.UnknownAccountException;
-import com.mallik.banktransfer.model.Account;
 import com.mallik.banktransfer.model.Transaction;
 import com.mallik.banktransfer.service.BankTransferServiceImpl;
 
@@ -31,36 +27,28 @@ class BanktransferApplicationTests {
 	@Autowired
 	private BankTransferServiceImpl bankTransferService;
 
-	// Below are the list of 2 test accounts added for testing purposes
-    List<Account> accounts = Stream.of(
-    				new Account(1L, "22", "Rama", new BigDecimal(250)), 
-    				new Account(2L, "33", "Krishna", new BigDecimal(100)))
-    																		.collect(Collectors.toList());
-    
     @Test
-    @DisplayName("Load Accounts And Verify They Exist")
-    public void loadAccountsAndVerify() {
-    	//saveAccounts is tested with 2 accounts
-    	bankTransferService.saveAccounts(accounts);
-        
+    @DisplayName("Get All Accounts")
+    public void testGetAllAccounts() {
     	//Test the getAllAccounts()
-    	assertEquals(2, bankTransferService.getAllAccounts().size());
-        
+    	assertEquals(3, bankTransferService.getAllAccounts().size());
+    }
+
+    @Test
+    @DisplayName("Find Account By Account Number")
+    public void testFindByAccountNumber() {
     	//Test findByAccountNumber()
-    	assertNotNull(bankTransferService.findByAccountNumber("22"));
+    	assertNotNull(bankTransferService.findByAccountNumber("12345"));
     }
     
     @Test
     @DisplayName("Perform a Transaction")
-    public void completeTransactionTest() {
+    public void testperformTransaction() {
     	
-    	//saveAccounts is tested with 2 accounts
-    	bankTransferService.saveAccounts(accounts);
-        
     	//Test performBankTransfer()
-    	Transaction transaction = new Transaction("22", "33", new BigDecimal(45));
+    	Transaction transaction = new Transaction("12345", "23456", new BigDecimal(45));
     	assertTrue(bankTransferService.performBankTransfer(transaction));
-	      
+
     	//Test getAllTransactions()
     	assertThat(bankTransferService.getAllTransactions().size()).isEqualTo(1);
     }
@@ -69,18 +57,16 @@ class BanktransferApplicationTests {
     @DisplayName("Negative Scenario - Insufficient Funds")
     public void testInsufficientFunds() {
     	
-    	//saveAccounts is tested with 2 accounts
-        bankTransferService.saveAccounts(accounts);
     	// Transfer more funds than the source account has
         assertThrows(InsufficientFundsException.class, () -> 
-        								bankTransferService.performBankTransfer(new Transaction("22", "33", new BigDecimal(45444))));
+        						bankTransferService.performBankTransfer(new Transaction("12345", "23456", new BigDecimal(45444))));
     }
     
     @Test
     @DisplayName("Negative Scenario - Unknown Account")
     public void testUnknownAccountException() {
     	assertThrows(UnknownAccountException.class, () -> 
-    									bankTransferService.findByAccountNumber("23434"));
+    							bankTransferService.findByAccountNumber("23434"));
 
     }
 
